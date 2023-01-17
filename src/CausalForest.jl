375b30b63104bd605e00f7a9dbe015b5c03b444b
Bérénice-Alexia Jocteur <berenice-alexia.jocteur@outlook.com>
2023-01-17 12:46:51 +0100

    Removed unused deps

--- src/CausalForest.jl
+++ src/CausalForest.jl
@@ -4,16 +4,9 @@ module CausalForest
 
 import Base: length, show, convert, promote_rule, zero
 using DecisionTree
-using DelimitedFiles
 using Random
 using Statistics
-using NonNegLeastSquares
-using LinearAlgebra
-using NearestNeighbors
-using Optim
-using MLBase
 import StatsBase
-import LinearAlgebra
 
 export TreeOOB, EnsembleOOB, apply_tree_oob, build_forest_oob, apply_forest_oob, load_data,
     importance, build_forest, apply_forest, build_forest_ols, apply_forest_ols
